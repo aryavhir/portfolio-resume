7ab633f2fecf340568bf941e9121bd29c1509fe5
aryavhir <aryavhirkoul1@gmail.com>
2025-01-23 17:04:45 +0530

    tre

--- public/hard.js
+++ public/hard.js
@@ -383,7 +383,6 @@ function createAdContainer() {
     for (let i = 0; i < totalImages; i++) {
         const img = document.createElement('img');
         Object.assign(img.style, {
-            width: '100%',
             borderRadius: '14px',
             minHeight: '135px',
             marginRight: '20px',
