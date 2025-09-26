db6adb47d275652f6a3c1f0b84f8bf93ec5c6bed
aryavhir <aryavhirkoul1@gmail.com>
2025-09-26 13:38:23 +0530

    projects

--- src/component/build-project.js
+++ src/component/build-project.js
@@ -3,10 +3,7 @@ import InfiniteMenu from "../asset/projects/InfiniteMenu";
 import project1 from '../asset/img/sourcebase.png'
 import project2 from '../asset/img/modelia.png'
 import project3 from '../asset/img/get-dots.png'
-import project4 from '../asset/img/cafe.png'
-import project7 from '../asset/img/car.png'
-import project6 from '../asset/img/cloth.png'
-import project5 from '../asset/img/news.png'
+
 import ScrambledText from '../asset/text-animation/ScrambledText';
 
 const items = [
@@ -28,30 +25,7 @@ const items = [
     title: "Freelance", 
     description: "Intro to sourcebase"
   },
-  {
-    image: project4,
-    link: "https://pub-2-hydro-test-7.vercel.app/", 
-    title: "Cafe",
-    description: "Dummy cafe website"
-  },
-  {
-    image: project5,
-    link: "https://news-app-rho-orpin.vercel.app/",
-    title: "News",
-    description: "Dummy news app"
-  },
-  {
-    image: project6,
-    link: "https://production-test-2.vercel.app/",
-    title: "Cloth",
-    description: "Dummy cloth store"
-  },
-  {
-    image: project7,
-    link: "https://pub1-website-1.vercel.app/",
-    title: "Car",
-    description: "Dummy car website"
-  }
+ 
 ];
 
 export const InfiniteSection = () => {
